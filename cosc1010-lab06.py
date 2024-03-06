dad4fd8de866d2d4677be6e1477acf5fd236aa44
mwelsh1043 <159499014+mwelsh1043@users.noreply.github.com>
2024-03-06 14:47:42 -0700

    Add files via upload

---
+++ cosc1010-lab06.py
@@ -0,0 +1,100 @@
+# Your Name Here
+# UWYO COSC 1010
+# Submission Date
+# Lab 06
+# Lab Section: 
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+
+random_string = """
+jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
+pkzwuiorngdfcsgqnlyifzyaivehpiyszykqprbcsobygzhadd
+yfddbulxmcnyvqhesmnybyuhxjqqmhdxwhcselasiayqhctnlw
+hakethjahqnvjdowhlyzosemxkbenestxgvgncmffkcxldcmkl
+itclmqdhrbdgzwtvdxwedcknbyaecvttjphtxubvhwvcvjqayy
+almxuxjbcmznnzekptfzbldsjwpvringlmalwufvlppeiendur
+dyophftqjkghhncwxoksqaqnpueudpygiytqgpcgjqsjbtbpzi
+vaeczmyicnednjjoxkpnjmpfbgyjnbfjlweqqppodfxfzzwkuf
+rldgryyhceuikimoavosuzuozthmatcgxcmkxnaxmsevkcumby
+spiajlbycvrluxdkfavxidzalxuixqkxiybhfuqhcvmrhzbzse
+idjwgwdwgfkyreozkyoxdvhixfejxjfgkkgobescboyfshiovu
+fxdyvfsnmjzsphgmtldlaoehofcspzujghcdcxzggvunpbtglr
+topplmkviuewwpoaplmbpgejmymxbyzzwbnujrlysszmxkjerb
+zpiewqvgopvhmmcgwcyvxvwhdvfgsrybcozhdtwujhdbxzznkc
+ergcqbetpgwrejuluqfxchlihunzbcdwboysjqenjxzbgqbycx
+dybxpyztjyxpkqfvxullzkedpkjjobhymfinpvprxejktyrpai
+ehjgwahpquzcmvclatdfcmattavoehnhnzveoxwnmnptxbvxto
+gpcobgzdhsjevhcohkltftmrqkosknkxeylhqxkkctbnusijgr
+uvecpbqmylqdaohkfaqbgeokyyipumjuaaayikdzyxfrpaieyo
+uxiosjwioebsjtslblfurgcodtyaggzovzfnnyjngawiwbbtqi
+kqqhnkwheolpqzasmsmbxqkeiqvogquobphewznfsnlkkizhca
+cbiyvxpmjxywqvzqtshfvnfbusphggexfqzepsrduvtovdsknl
+ztyuwugprkhbmktfvrenbmqgdjwnkeugtojrpqfmjhtrlcqcpq
+pwsguedzgvktpwbqkhkueymjtxbvzmdfjopzkygujrjdtogssg
+cxczryuqhhgjlpultkoffescpzyjrfqqabnhkfdnhkylpjamxk
+uxidjkqdrkxqjqjtflebvwhcvqjciykzhrvppvxhvpedgznwty
+kujglixooczrhxziasjxddfcghzlwrqcyiilpruhdfvitewxzg
+dzcvmvnoskchscgoqfsojfvahlwkrslzeirlblseplcmpmbmum
+ibrdamvqfstydtjopdkdcbnnmpifxckozyxzluhcqbqtpismog
+ulufaajxvuizvdzioxfvypxovptkibcrjvfidomejknuggfrtp
+kptwffersvqjknemkejsgspckwqisdcliuezhbeqpwgrjcqajl
+huobykkbujmyuuinbwdklqfhvakyozzsxghfyownjjwqtkxgkf
+ipdbjzxfogozstfsektujsvklrvecditiectuvtfibohmxxzna
+cpqzeoburtquuizhypugnkvuwbdxnraareqkofhfjobrpcsuxq
+nbafxlkuafbfsiuyrxdusqyasqyrwhdjrukgxdackumvairlgn
+fjhenwbrdghbevgqbybpwncclolgqyuhallbqtzdywbvlzwtil
+jctmsxjortnxvlbhuhkblppewjhqjzxrwgftlturxjuwfoaqpp
+sgfnxwxolkbrpdmpniitoljzaxabgtnelrmryetxqypwrjdyjc
+zipwbdpbazxpesmrcfuikeamtlsrgxrhzfytecenyydeemrhxj
+gmdruhillntvpadzbroyygydpmonwuakruvxbdrqhtrjvoqsin
+gjbarzvuqplmsmbwtqfghteoknbxmaokwlqqfoblmzsxczjzfj
+mzmawtarjdtgongqqufhhdjwcinhlxcsgoltjycxrkloqozxoi
+crlfmgflzzxgiiliqlksxyaydsohhahzxtsufzppftvgbpsdlx
+ertfmbothijzrrdvfrnsohnwulcxvcvxngvmznhazxrgdsugij
+fracotpirvqemsiuualpkpvtmtgchmowkmvoolrjfblrtwkmtr
+xhawucytgwlahddkhxxfublukkdldpovqokntydhzzrxiisdwu
+ujrkoewqoflyebogbwgdhriwkkoiofwtjlhxxtmzkklzbcmxhv
+lrslowamkcwolbcgfkfciegdwqskuazxnycqkkggzsowcmafay
+ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
+zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
+"""
+random_string = random_string.replace("\n","") #remove all newline characters
+print(len(random_string)) # Print out the size for reference 
+
+# Above is a string with 2500 characters.
+# Create a program that goes through and counts the occurrence of each character, excluding \n
+# Output: each letter and its corresponding occurrence in alphabetical order
+# Output which letter occurred the most 
+# Output which letter occurred the least 
+# Output what the percentage of the string each character is, again in alphabetical
+
+#Tips and trick:
+# You can iterate through strings like you would a list
+# All characters are lowercase 
+# Each letter will be PAIRED with its corresponding value 
+# That is to say, this is a great use of dictionaries
+    # You will  need to add the letter to the dictionary on first occurrence 
+    # Then increment its corresponding count 
+
+
+#Load all the elements into a dictionary
+#Will need to first declare a dictionary 
+
+# Output: each letter and its corresponding occurrence in alphabetical order
+
+print("*"*75)
+# Output which letter occurred the most 
+
+most_occurred = ""
+least_occurred = ""
+
+print(f"The letter that occurred the most is {most_occurred}")
+print("*"*75)
+# Output which letter occurred the least 
+print(f"The letter that occurred the most is {least_occurred}")
+print("*"*75)
+
+# Output what the percentage of the string each character is, again in alphabetical
